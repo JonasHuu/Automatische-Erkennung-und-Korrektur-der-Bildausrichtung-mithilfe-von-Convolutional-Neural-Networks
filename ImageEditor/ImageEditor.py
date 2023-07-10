df55440525b017bf827997c82dd248cc6452faf7
jonashuurdeman@gmx.de <jonashuurdeman@gmx.de>
2023-07-10 08:11:27 +0200

    added dark theme and fixed progress dialogue

--- ImageEditor/ImageEditor.py
+++ ImageEditor/ImageEditor.py
@@ -22,6 +22,89 @@ from PyQt6.QtWidgets import (
     QProgressDialog,
     QCheckBox
 )
+import qdarktheme
+dark_theme = """
+    QWidget {
+        background-color: #333333;
+        color: #ffffff;
+        border: none;
+    }
+    QPushButton {
+        background-color: #4d4d4d;
+        border: 1px solid #4d4d4d;
+        border-radius: 4px;
+        color: #ffffff;
+        padding: 5px;
+    }
+    QPushButton:hover {
+        background-color: #5a5a5a;
+        border: 1px solid #5a5a5a;
+    }
+    QCheckBox {
+        color: #ffffff;
+    }
+    QLineEdit {
+        background-color: #4d4d4d;
+        border: 1px solid #4d4d4d;
+        color: #ffffff;
+        padding: 5px;
+    }
+    QTextEdit {
+        background-color: #4d4d4d;
+        border: 1px solid #4d4d4d;
+        color: #ffffff;
+        padding: 5px;
+    }
+    QProgressBar {
+        border: 1px solid #444444;
+        border-radius: 7px;
+        background-color: #2e2e2e;
+        text-align: center;
+        font-size: 10pt;
+        color: white;
+    }
+    QProgressBar::chunk {
+        background-color: #3a3a3a;
+        width: 5px;
+    }
+    QScrollBar:vertical {
+        border: none;
+        background-color: #3a3a3a;
+        width: 10px;
+        margin: 16px 0 16px 0;
+    }
+    QScrollBar::handle:vertical {
+        background-color: #444444;
+        border-radius: 5px;
+    }
+    QScrollBar:horizontal {
+        border: none;
+        background-color: #3a3a3a;
+        height: 10px;
+        margin: 0px 16px 0 16px;
+    }
+    QScrollBar::handle:horizontal {
+        background-color: #444444;
+        border-radius: 5px;
+    }
+    QTabWidget {
+        background-color: #2e2e2e;
+        border: none;
+    }
+    QTabBar::tab {
+        background-color: #2e2e2e;
+        color: #b1b1b1;
+        padding: 8px 20px;
+        border-top-left-radius: 5px;
+        border-top-right-radius: 5px;
+        border: none;
+    }
+
+    QTabBar::tab:selected, QTabBar::tab:hover {
+        background-color: #3a3a3a;
+        color: white;
+    }
+"""
 class Dialog(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -61,6 +144,44 @@ class CorrectRotationThread(QThread):
             self.model = load_model('./model/efficientnetv2_sv_open_images.hdf5', custom_objects={'angle_error': angle_error})
 
         process_images(self.model, self.selected_files, output_path, batch_size=64, crop=True)
+
+        if self.running:
+            self.rotation_completed.emit()
+
+class CorrectRotationThread_main(QThread):
+    rotation_completed = pyqtSignal()
+
+    def __init__(self, selected_files, original_angle,model, parent=None):
+        super().__init__(parent)
+        self.filename = selected_files
+        self.model = model
+        self.running = True
+        self.rotation_angle_orig = original_angle
+        self.rotation_angle = 0
+
+    def quit(self):
+        self.running = False
+        super().quit()
+
+    def run(self):
+        if self.model is None:
+                self.model = load_model('./model/efficientnetv2_sv_open_images.hdf5', custom_objects={'angle_error': angle_error})
+
+        #transform = QTransform()
+        predictions = self.model.predict(
+            RotNetDataGenerator(
+                self.filename,
+                input_shape=(224, 224, 3),
+                batch_size=64,
+                one_hot=True,
+                preprocess_func=preprocess_input,
+                rotate=True,
+                crop_largest_rect=True,
+                crop_center=True,
+                angle=self.rotation_angle_orig
+            )
+        )
+        self.rotation_angle = np.argmax(predictions, axis=1) - self.rotation_angle_orig
 
         if self.running:
             self.rotation_completed.emit()
@@ -211,7 +332,6 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi("form.ui", self)
-        
         # Connect signals and slots
         self.autocorrectButton.clicked.connect(self.autocorrect)
         self.open_button.triggered.connect(self.load_image)
@@ -220,6 +340,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.save_button.clicked.connect(self.save_rotated_image)
         self.horizontalSlider_2.valueChanged.connect(self.update_rotation)
         self.horizontalSlider.valueChanged.connect(self.update_rotation_orig)
+        self.actionDark_mode.triggered.connect(self.change_dark_mode)
+        self.actionLight_mode.triggered.connect(self.change_light_mode)
+        self.actionAuto.triggered.connect(self.change_auto_mode)
         #self.correct_rotation_button.clicked.connect(self.start_rotation_correction)
         # Initialize variables
         self.original_image = None
@@ -229,6 +352,13 @@ class MainWindow(QtWidgets.QMainWindow):
         self.filename = None
         self.remove_black_border = False
         self.model = None
+
+    def change_dark_mode(self):
+        qdarktheme.setup_theme("dark")
+    def change_light_mode(self):
+        qdarktheme.setup_theme("light")
+    def change_auto_mode(self):
+        qdarktheme.setup_theme("auto")
     def load_image(self):
         file_dialog = QFileDialog()
         file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
@@ -275,12 +405,18 @@ class MainWindow(QtWidgets.QMainWindow):
             progress_dialog.setRange(0, 0)  # Indeterminate progress
             progress_dialog.show()
 
-            QCoreApplication.processEvents()  # Allow the progress dialog to be displayed
-
-            if self.model is None:
-                self.model = load_model('./model/efficientnetv2_sv_open_images.hdf5', custom_objects={'angle_error': angle_error})
+            #QCoreApplication.processEvents()  # Allow the progress dialog to be displayed
+            # Start the rotation correction in a separate thread
+            self.rotation_thread = CorrectRotationThread_main(self.filename, self.rotation_angle_orig,self.model)
+            self.rotation_thread.started.connect(progress_dialog.show)
+            self.rotation_thread.finished.connect(progress_dialog.close)
+            self.rotation_thread.finished.connect(lambda: self.correct_rotation_button.setEnabled(True))
+            self.rotation_thread.start()
+            #if self.model is None:
+            #    self.model = load_model('./model/efficientnetv2_sv_open_images.hdf5', custom_objects={'angle_error': angle_error})
 
             transform = QTransform()
+            '''
             predictions = self.model.predict(
                 RotNetDataGenerator(
                     self.filename,
@@ -295,7 +431,8 @@ class MainWindow(QtWidgets.QMainWindow):
                 )
             )
             self.rotation_angle = np.argmax(predictions, axis=1) - self.rotation_angle_orig
-            transform.rotate(self.rotation_angle)
+            '''
+            transform.rotate(self.rotation_thread.rotation_angle)
             self.rotated_image = self.original_image.transformed(transform)
             self.display_images()
 
@@ -366,8 +503,11 @@ class MainWindow(QtWidgets.QMainWindow):
                            
                         else:
                             self.rotated_image.save(filename)
-    
+
+qdarktheme.enable_hi_dpi()
 app = QtWidgets.QApplication(sys.argv)
+#app.setStyleSheet(dark_theme)
+qdarktheme.setup_theme("auto")
 window = MainWindow()
 window.show()
 app.exec()
