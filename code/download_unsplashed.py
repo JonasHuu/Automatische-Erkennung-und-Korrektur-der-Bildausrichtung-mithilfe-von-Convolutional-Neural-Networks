d3bebdd3fb1409933f1dccdd7fe591fffd4bb689
jonashuurdeman@gmx.de <jonashuurdeman@gmx.de>
2023-07-14 10:40:05 +0200

    added multithreading

--- code/download_unsplashed.py
+++ code/download_unsplashed.py
@@ -7,6 +7,14 @@ urls = photos['photo_image_url']
 
 import urllib.request
 indexes = np.random.choice(24000, 1000)
-for i in indexes:
-    urllib.request.urlretrieve(urls[i] + "?ixid=2yJhcHBfaWQiOjEyMDd9&&fm=jpg&w=400&fit=max", "./data/unsplashed_images/" + str(i) + ".png")
-  
+#for i in indexes:
+#    urllib.request.urlretrieve(urls[i] + "?ixid=2yJhcHBfaWQiOjEyMDd9&&fm=jpg&w=400&fit=max", "./data/unsplashed_images/" + str(i) + ".png")
+
+from concurrent.futures import ThreadPoolExecutor
+
+def download(ind):
+    urllib.request.urlretrieve(urls[ind] + "?ixid=2yJhcHBfaWQiOjEyMDd9&&fm=jpg&w=400&fit=max", "./data/unsplashed_images/" + str(ind) + ".png")
+    print("Downloaded {}".format(ind))
+
+with ThreadPoolExecutor(max_workers=32) as executor:
+    executor.map(download,indexes)
