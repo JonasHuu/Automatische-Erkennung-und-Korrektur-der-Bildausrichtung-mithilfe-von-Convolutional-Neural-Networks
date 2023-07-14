5f0df94a1288d37f62de10807e6909924734f24c
JonasHuu <96773595+JonasHuu@users.noreply.github.com>
2023-07-14 10:43:04 +0200

    Update download_unsplashed.py

--- code/download_unsplashed.py
+++ code/download_unsplashed.py
@@ -1,14 +1,13 @@
 import numpy as np
 import pandas as pd
+import os
+import urllib.request
 
+os.makedirs(os.path.join('data','unsplashed_images'))
 df = pd.read_csv('./code/photos.tsv000', sep='\t', header=0)
 photos = df
 urls = photos['photo_image_url']
-
-import urllib.request
 indexes = np.random.choice(24000, 1000)
-#for i in indexes:
-#    urllib.request.urlretrieve(urls[i] + "?ixid=2yJhcHBfaWQiOjEyMDd9&&fm=jpg&w=400&fit=max", "./data/unsplashed_images/" + str(i) + ".png")
 
 from concurrent.futures import ThreadPoolExecutor
 
