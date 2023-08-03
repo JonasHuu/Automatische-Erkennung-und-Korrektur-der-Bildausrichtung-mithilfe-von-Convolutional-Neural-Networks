088a9abd0ed209bd65697efe359e3592dfeb15af
JonasHuu <96773595+JonasHuu@users.noreply.github.com>
2023-08-03 15:34:52 +0200

    Update csv_to_txt.py

--- code/csv_to_txt.py
+++ code/csv_to_txt.py
@@ -5,7 +5,6 @@ df = pd.read_csv('train-images-boxable-with-rotation.csv')
 ImageID = df["ImageID"]
 Subset = df["Subset"]
 file = open("train.txt", "w")
-#ran_nums = random.sample(range(1,100000), 1500)
 
 for i in range(1,1000000):
     file.write(Subset[i] + '/' +  ImageID[i] + "\n")
